e85c97b4240fe769d61ba06bf5928af5bafde179
robertlech <robert@kippiz.de>
2015-12-16 19:49:12 +0100

    updated some stuff

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+    id: 'io.blackboard',
+    name: 'BlackBoard',
+    description: '',
+    author: 'Robert Lech',
+    email: 'robert.lech@icloud.com',
+    website: 'http://example.com'
+});
+App.accessRule('*');
